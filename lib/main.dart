b3d60fde43889be276a5c95336f479e1ed8b16db
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-05-31 14:08:28 +0530

    Redesigned

--- lib/main.dart
+++ lib/main.dart
@@ -20,126 +20,114 @@ class Piano extends StatelessWidget {
           centerTitle: true,
         ),
         body: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          // crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Row(
-              // mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(
-                  height: 200,
-                  child: Expanded(
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white),
-                          onPressed: () {
-                            print("Play");
-                            final player = AudioPlayer();
-                            player.play(AssetSource('note1.wav'));
-                          },
-                          child: const Text(
-                            "1",
-                            style: TextStyle(color: Colors.teal, fontSize: 20),
-                          ))),
+                ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                  onPressed: () {
+                    print("Play");
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note1.wav'));
+                  },
+                  child: const Text(
+                    "1",
+                    style: TextStyle(color: Colors.teal, fontSize: 20),
+                  ),
                 ),
-                SizedBox(
-                  height: 200,
-                  child: Expanded(
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.black),
-                          onPressed: () {
-                            print("Play");
-                            final player = AudioPlayer();
-                            player.play(AssetSource('note2.wav'));
-                          },
-                          child: const Text(
-                            "2",
-                            style: TextStyle(color: Colors.teal, fontSize: 20),
-                          ))),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                  ),
+                  onPressed: () {
+                    print("Play");
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note2.wav'));
+                  },
+                  child: const Text(
+                    "2",
+                    style: TextStyle(color: Colors.teal, fontSize: 20),
+                  ),
                 ),
-                SizedBox(
-                  height: 200,
-                  child: Expanded(
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white),
-                          onPressed: () {
-                            print("Play");
-                            final player = AudioPlayer();
-                            player.play(AssetSource('note3.wav'));
-                          },
-                          child: const Text(
-                            "3",
-                            style: TextStyle(color: Colors.teal, fontSize: 20),
-                          ))),
+                ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                  onPressed: () {
+                    print("Play");
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note3.wav'));
+                  },
+                  child: const Text(
+                    "3",
+                    style: TextStyle(color: Colors.teal, fontSize: 20),
+                  ),
                 ),
-                SizedBox(
-                  height: 200,
-                  child: Expanded(
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.black),
-                          onPressed: () {
-                            print("Play");
-                            final player = AudioPlayer();
-                            player.play(AssetSource('note4.wav'));
-                          },
-                          child: const Text(
-                            "4",
-                            style: TextStyle(color: Colors.teal, fontSize: 20),
-                          ))),
+                ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                  onPressed: () {
+                    print("Play");
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note4.wav'));
+                  },
+                  child: const Text(
+                    "4",
+                    style: TextStyle(color: Colors.teal, fontSize: 20),
+                  ),
                 ),
-                SizedBox(
-                  height: 200,
-                  child: Expanded(
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white),
-                          onPressed: () {
-                            print("Play");
-                            final player = AudioPlayer();
-                            player.play(AssetSource('note5.wav'));
-                          },
-                          child: const Text(
-                            "5",
-                            style: TextStyle(color: Colors.teal, fontSize: 20),
-                          ))),
-                ),
-                SizedBox(
-                  height: 200,
-                  child: Expanded(
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.black),
-                          onPressed: () {
-                            print("Play");
-                            final player = AudioPlayer();
-                            player.play(AssetSource('note6.wav'));
-                          },
-                          child: const Text(
-                            "6",
-                            style: TextStyle(color: Colors.teal, fontSize: 20),
-                          ))),
-                ),
-                SizedBox(
-                  height: 200,
-                  child: Expanded(
-                      child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.white),
-                          onPressed: () {
-                            print("Play");
-                            final player = AudioPlayer();
-                            player.play(AssetSource('note7.wav'));
-                          },
-                          child: const Text(
-                            "7",
-                            style: TextStyle(color: Colors.teal, fontSize: 20),
-                          ))),
+                const SizedBox(
+                  height: 50,
                 ),
               ],
-            )
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                  onPressed: () {
+                    print("Play");
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note5.wav'));
+                  },
+                  child: const Text(
+                    "5",
+                    style: TextStyle(color: Colors.teal, fontSize: 20),
+                  ),
+                ),
+                ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                  onPressed: () {
+                    print("Play");
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note6.wav'));
+                  },
+                  child: const Text(
+                    "6",
+                    style: TextStyle(color: Colors.teal, fontSize: 20),
+                  ),
+                ),
+                ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                  onPressed: () {
+                    print("Play");
+                    final player = AudioPlayer();
+                    player.play(AssetSource('note7.wav'));
+                  },
+                  child: const Text(
+                    "7",
+                    style: TextStyle(color: Colors.teal, fontSize: 20),
+                  ),
+                )
+              ],
+            ),
           ],
         ),
       ),
