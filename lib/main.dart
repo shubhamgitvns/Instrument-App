74bb9e834866fc744be18f56d3ba99bcf5feeced
shubhamgitvns <93187281+shubhamgitvns@users.noreply.github.com>
2023-06-01 16:44:35 +0530

    bubble shift in background

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -29,14 +30,8 @@ void main() async {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amber,
-          title: Text("Piano by shubham"),
-          centerTitle: true,
-        ),
         body: Container(
-
-           color: const Color(0xddd5000),
+          color: const Color(0xFF15202D),
           child: const SizedBox.expand(
             child: VariousDiscs(50),
           ),
@@ -64,217 +59,264 @@ class _VariousDiscsState extends State<VariousDiscs> {
     _makeDiscs();
   }
 
-  void _makeDiscs() async {
-    _discs.clear();
-    for (int i = 0; i < widget.numberOfDiscs; i++) {
-      _discs.add(DiscData());
-    }
-    await Future.delayed(const Duration(seconds: 2));
+  void _makeDiscs() {
     _discs.clear();
     for (int i = 0; i < widget.numberOfDiscs; i++) {
       _discs.add(DiscData());
     }
   }
-
+double buttonheight=150;
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Row(
-          children:  [
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+      Expanded(
+        child: SizedBox(
 
-    RichText(
-    text: TextSpan(
-
-      style: DefaultTextStyle.of(context).style,
-      children: const <TextSpan>[
-        TextSpan(text: 'Created by', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.black)),
-        TextSpan(text: ' Shubham Mourya',style: TextStyle(fontWeight: FontWeight.bold,fontStyle: FontStyle.italic,fontSize: 30,color: Colors.cyan)),
-
-
-          ],
-        ),
-    ),
-        ]
-        ),
-        Center(
-          child: Row(
+          height: 300,
+          child: Stack(
             children: [
-              Expanded(
-                  child: SizedBox(
-                height: 250,
-                child: ElevatedButton(
-                  style:
-                      ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                  onPressed: () {
-                    setState(
-                      () {
-                        _makeDiscs();
-                        // await Future.delayed(Duration(seconds: 2));
-                        //_makeDiscs();
-                      },
-                    );
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note1.wav'));
 
-                    print("Play");
-                  },
-                  child: const Text(
-                    "",
-                    style: TextStyle(color: Colors.teal, fontSize: 20),
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                    style: DefaultTextStyle.of(context).style,
+                    children: const <TextSpan>[
+                      TextSpan(
+                          text: 'Created by',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.black)),
+                      TextSpan(
+                          text: ' Shubham Mourya',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic,
+                              fontSize: 30,
+                              color: Colors.cyan)),
+                    ],
                   ),
                 ),
-              )),
-              Expanded(
-                  child: SizedBox(
-                height: 250,
-                child: ElevatedButton(
-                  style:
-                      ElevatedButton.styleFrom(backgroundColor: Colors.white),
-                  onPressed: () {
-                    setState(() {
-                      _makeDiscs();
-                    });
-                    print("Play");
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note2.wav'));
-                  },
-                  child: const Text(
-                    "",
-                    style: TextStyle(color: Colors.teal, fontSize: 20),
-                  ),
-                ),
-              )),
-              Expanded(
-                  child: SizedBox(
-                height: 250,
-                child: ElevatedButton(
-                  style:
-                      ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                  onPressed: () {
-                    setState(() {
-                      _makeDiscs();
-                    });
-                    print("Play");
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note3.wav'));
-                  },
-                  child: const Text(
-                    "",
-                    style: TextStyle(color: Colors.teal, fontSize: 20),
-                  ),
-                ),
-              )),
-              Expanded(
-                  child: SizedBox(
-                height: 250,
-                child: ElevatedButton(
-                  style:
-                      ElevatedButton.styleFrom(backgroundColor: Colors.white),
-                  onPressed: () {
-                    setState(() {
-                      _makeDiscs();
-                    });
-                    print("Play");
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note4.wav'));
-                  },
-                  child: const Text(
-                    "",
-                    style: TextStyle(color: Colors.teal, fontSize: 20),
-                  ),
-                ),
-              )),
-              Expanded(
-                  child: SizedBox(
-                height: 250,
-                child: ElevatedButton(
-                  style:
-                      ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                  onPressed: () {
-                    setState(() {
-                      _makeDiscs();
-                    });
-                    print("Play");
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note5.wav'));
-                  },
-                  child: const Text(
-                    "",
-                    style: TextStyle(color: Colors.teal, fontSize: 20),
-                  ),
-                ),
-              )),
-              Expanded(
-                  child: SizedBox(
-                height: 250,
-                child: ElevatedButton(
-                  style:
-                      ElevatedButton.styleFrom(backgroundColor: Colors.white),
-                  onPressed: () {
-                    setState(() {
-                      _makeDiscs();
-                    });
-                    print("Play");
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note6.wav'));
-                  },
-                  child: const Text(
-                    "",
-                    style: TextStyle(color: Colors.teal, fontSize: 20),
-                  ),
-                ),
-              )),
-              Expanded(
-                  child: SizedBox(
-                height: 250,
-                child: ElevatedButton(
-                  style:
-                      ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                  onPressed: () {
-                    setState(() {
-                      _makeDiscs();
-                    });
-                    print("Play");
-                    final player = AudioPlayer();
-                    player.play(AssetSource('note7.wav'));
-                  },
-                  child: const Text(
-                    "",
-                    style: TextStyle(color: Colors.teal, fontSize: 20),
-                  ),
-                ),
-              ))
+              ),
+              GestureDetector(
+                onTap: () => setState(() {
+                  _makeDiscs();
+                }),
+                child: Stack(children: [
+                  for (final disc in _discs)
+                    Positioned.fill(
+                      child: AnimatedAlign(
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.easeInOut,
+                        alignment: disc.alignment,
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 500),
+                          decoration: BoxDecoration(
+                            color: disc.color,
+                            shape: BoxShape.circle,
+                          ),
+                          height: disc.size,
+                          width: disc.size,
+                        ),
+                      ),
+                    ),
+                ]),
+              ),
             ],
           ),
         ),
-        GestureDetector(
-          onTap: () => setState(() {
-            _makeDiscs();
-          }),
-          child: Stack(
+      ),
+      Column(
+        children: [
+          Row(
             children: [
-              for (final disc in _discs)
-                Positioned.fill(
-                  child: AnimatedAlign(
-                    duration: const Duration(milliseconds: 500),
-                    curve: Curves.easeInOut,
-                    alignment: disc.alignment,
-                    child: AnimatedContainer(
-                      duration: const Duration(milliseconds: 500),
-                      decoration: BoxDecoration(
-                        color: disc.color,
-                        shape: BoxShape.circle,
-                      ),
-                      height: disc.size,
-                      width: disc.size,
+              Expanded(
+
+                child: SizedBox(
+                  height: buttonheight,
+                  child: ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                    onPressed: () {
+                      setState(
+                        () {
+                          _makeDiscs();
+                        },
+                      );
+                      final player = AudioPlayer();
+                      player.play(AssetSource('note1.wav'));
+                      print("Play");
+                    },
+                    child: const Text(
+                      "",
+                      style: TextStyle(color: Colors.teal, fontSize: 20),
                     ),
                   ),
                 ),
+              ),
+              Expanded(
+                child: SizedBox(
+                  height: buttonheight,
+                  child: ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                    onPressed: () {
+                      setState(
+                        () {
+                          _makeDiscs();
+                          // await Future.delayed(Duration(seconds: 2));
+                          //_makeDiscs();
+                        },
+                      );
+                      final player = AudioPlayer();
+                      player.play(AssetSource('note2.wav'));
+
+                      print("Play");
+                    },
+                    child: const Text(
+                      "",
+                      style: TextStyle(color: Colors.teal, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: SizedBox(
+                  height: buttonheight,
+                  child: ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                    onPressed: () {
+                      setState(
+                        () {
+                          _makeDiscs();
+                          // await Future.delayed(Duration(seconds: 2));
+                          //_makeDiscs();
+                        },
+                      );
+                      final player = AudioPlayer();
+                      player.play(AssetSource('note3.wav'));
+
+                      print("Play");
+                    },
+                    child: const Text(
+                      "",
+                      style: TextStyle(color: Colors.teal, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: SizedBox(
+                  height: buttonheight,
+                  child: ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                    onPressed: () {
+                      setState(
+                        () {
+                          _makeDiscs();
+                          // await Future.delayed(Duration(seconds: 2));
+                          //_makeDiscs();
+                        },
+                      );
+                      final player = AudioPlayer();
+                      player.play(AssetSource('note4.wav'));
+
+                      print("Play");
+                    },
+                    child: const Text(
+                      "",
+                      style: TextStyle(color: Colors.teal, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: SizedBox(
+                  height: buttonheight,
+                  child: ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                    onPressed: () {
+                      setState(
+                        () {
+                          _makeDiscs();
+                          // await Future.delayed(Duration(seconds: 2));
+                          //_makeDiscs();
+                        },
+                      );
+                      final player = AudioPlayer();
+                      player.play(AssetSource('note5.wav'));
+
+                      print("Play");
+                    },
+                    child: const Text(
+                      "",
+                      style: TextStyle(color: Colors.teal, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: SizedBox(
+                  height: buttonheight,
+                  child: ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                    onPressed: () {
+                      setState(
+                        () {
+                          _makeDiscs();
+                          // await Future.delayed(Duration(seconds: 2));
+                          //_makeDiscs();
+                        },
+                      );
+                      final player = AudioPlayer();
+                      player.play(AssetSource('note6.wav'));
+
+                      print("Play");
+                    },
+                    child: const Text(
+                      "",
+                      style: TextStyle(color: Colors.teal, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: SizedBox(
+                  height: buttonheight,
+                  child: ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                    onPressed: () {
+                      setState(
+                        () {
+                          _makeDiscs();
+                          // await Future.delayed(Duration(seconds: 2));
+                          //_makeDiscs();
+                        },
+                      );
+                      final player = AudioPlayer();
+                      player.play(AssetSource('note7.wav'));
+
+                      print("Play");
+                    },
+                    child: const Text(
+                      "",
+                      style: TextStyle(color: Colors.teal, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
             ],
-          ),
-        ),
-      ],
-    );
+          )
+        ],
+      )
+    ]);
   }
 }
